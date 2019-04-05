bd13d3ff08a53652f589c56bc131e872de7aff91
Michał Wierzbiński <michal@envokeit.com>
2019-04-05 15:23:10 +0200

    Initial commit

---
+++ Sample.Web/Controllers/SampleController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+using System.Web.Mvc;
+using Sample.Web.Models;
+
+namespace Sample.Web.Controllers
+{
+    public class SampleController : Controller
+    {
+        // GET: Sample
+        public ActionResult Index(SampleViewModel model = null)
+        {
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult ProcessDate(SampleViewModel sampleViewModel)
+        {
+            var success = DateTime.TryParseExact(sampleViewModel.Date, "yyyy/MM/ddThh:mm:ss",
+                CultureInfo.InvariantCulture, DateTimeStyles.AdjustToUniversal, out var dateToProcess);
+
+            if (!success)
+            {
+                // Place your logic here.
+            }
+
+            var timeDiff = DateTime.Now - dateToProcess;
+
+            var result =
+                $"It's been {timeDiff.Days} days, {timeDiff.Hours} hours, {timeDiff.Minutes} minutes and {timeDiff.Seconds} seconds since that day... Good times... Good times...";
+
+            sampleViewModel.Result = result;
+
+            return RedirectToAction("Index", sampleViewModel);
+        }
+    }
+}
---
+++ Sample.Web/Models/SampleViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sample.Web.Models
+{
+    public class SampleViewModel
+    {
+        public string Result { get; set; }
+
+        [Required]
+        public string Date { get; set; }
+    }
+}
